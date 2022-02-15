f970c8bf9447ee76a070942ecfb3ee3ccb11dfed
romeu <passadore.neto@outlook.com>
2022-02-15 17:39:34 -0300

    segundo commit

--- app/src/main/java/br/senai/sp/cotia/calcsalario/MainActivity.java
+++ app/src/main/java/br/senai/sp/cotia/calcsalario/MainActivity.java
@@ -23,20 +23,60 @@ private Button bt_resultado;
          vaRefeicao = findViewById(R.id.vaRefeicao);
          resultado = findViewById(R.id.resultado);
          bt_resultado = findViewById(R.id.bt_resultado);
+
          bt_resultado.setOnClickListener( v -> {
              if (salariobruto.getText().toString().isEmpty()){
                  salariobruto.setError(getString(R.string.validadeSalario));
              }else if (depen.getText().toString().isEmpty()) {
-                 depen.setError(getString(R.string.validadeSalario));
+                 depen.setError(getString(R.string.Numdep));
              }else if(tpPlanoSaude.getText().toString().isEmpty()) {
-                 tpPlanoSaude.setError(getString(R.string.validadeSalario));
+                 tpPlanoSaude.setError(getString(R.string.TipoPlano));
              }else if(vlTransporte.getText().toString().isEmpty()) {
-                 vlTransporte.setError(getString(R.string.validadeSalario));
+                 vlTransporte.setError(getString(R.string.Vatrans));
              }else if(vaRefeicao.getText().toString().isEmpty()) {
-                 vaRefeicao.setError(getString(R.string.validadeSalario));
+                 vaRefeicao.setError(getString(R.string.VaRef));
              }else {
+                 double  salarioBruto ,resPlanosaude =0;
+                 double res ;
+                 int dePendete;
+                 String tipoPlanosaude,tipovaliTranspor,valirefeicao ;
+                 salarioBruto = Double.parseDouble(salariobruto.getText().toString());
+                  dePendete = Integer.parseInt(depen.getText().toString());
+                  tipoPlanosaude =  tpPlanoSaude.getText().toString();
+                  if(tipoPlanosaude == "Standard"){
+                      if(salarioBruto <= 3000.00){
+                       resPlanosaude = 60.00;
+                      }else {
+                          resPlanosaude = 80.00;
+                      }
+                  }else if(tipoPlanosaude == "basico"){
+                      if(salarioBruto <= 3000.00){
+                          resPlanosaude = 80.00;
+                      }else {
+                          resPlanosaude = 110.00;
+                      }
+
+                  }else if(tipoPlanosaude == "Super"){
+                      if(salarioBruto <= 3000.00){
+                          resPlanosaude = 95.00;
+                      }else {
+                          resPlanosaude = 135.00;
+                      }
+
+                  }else if(tipoPlanosaude == "Master"){
+                      if(salarioBruto <= 3000.00){
+                          resPlanosaude = 130.00;
+                      }else {
+                          resPlanosaude = 180.00;
+                      }
+
+                  }
+                  
+
+
 
              }
+
          });
     }
 }
